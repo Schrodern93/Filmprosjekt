c370e5c6a40c8bb6b1dd92c74484def9cbe27cdc
Joranstr <55440769+Joranstr@users.noreply.github.com>
2019-11-13 15:03:31 +0100

    Nye test documenter

--- Test Filer/Jøran's Test filer/LeggInnFilmTest.js
+++ Test Filer/Jøran's Test filer/LeggInnFilmTest.js
@@ -2,9 +2,22 @@
 function showAddMovIes() {
     let html = `
                 <tr>
-                    <th></th>
-                    <th></th>
+                    <th>Oppgave</th>
+                    <th>Hvem</th>
+                    <th>Frist</th>
+                    <th>Gjort</th>
                     <th></th>
                 </tr>
+                <tr>
+                    <th><input id="movie" type="text"/></th>
+                    <th><input id="genre" type="dropdown"/></th>
+                    <th><input id="releaseDate" type="dropdown"/></th>
+                    <th><input id="Favorite" type="chechbox"/></th>
+                    <th><input id="addMovieButton" type="button"/></th>
+                </tr>
                 `
+}
+
+function createHtmlRow() {
+
 }
