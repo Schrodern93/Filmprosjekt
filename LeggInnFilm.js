dc9933807951a910f5e3ffe04bb899a71ad8436f
Joranstr <55440769+Joranstr@users.noreply.github.com>
2019-11-14 14:45:21 +0100

    Update LeggInnFilm.js

--- LeggInnFilm.js
+++ LeggInnFilm.js
@@ -1,1 +1,10 @@
 // JavaScript source code
+function showAddMovIes() {
+    let html = `
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                `
+}
