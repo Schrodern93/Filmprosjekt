aac0720dd448b09982af8141a11ddc52d54caa66
ajja94 <anja@getacademy.no>
2019-11-15 14:58:35 +0100

    :+1:

--- sjangerListe.js
+++ sjangerListe.js
@@ -1,11 +1,89 @@
 // JavaScript source code
 load();
 //for (i = 0; i < FilmData.length; i++) {
-let alle = all[{ FilmData.Tittel, FilmData.Sjanger, FilmData.Spilletid, FilmData.Favoritt }];
+//let alle = all[{ FilmData.Tittel, FilmData.Sjanger, FilmData.Spilletid, FilmData.Favoritt }];
 
-   // alle = FilmData;
+//   // alle = FilmData;
+////}
+//console.log(alle);
+
+// model
+let currentMovies = [];
+let selectedGenre;
+let genres = extractGenres();
+
+// view
+show();
+function show() {
+    let optionsHtml = '';
+    for (let genre of genres) {
+        let selected = selectedGenre == genre ? 'selected' : '';
+        optionsHtml += `<option ${selected}>${genre}</option>`;
+    }
+
+    let moviesHtml = '';
+    for (let movie of currentMovies) {
+        moviesHtml += `<tr>
+                                    <td>${movie.movie_title}</td>
+                                    <td>${movie.genres}</td>
+                                    <td>${movie.director_name}</td>
+                                    <td>${movie.title_year}</td>
+                        </tr>`;
+    }
+
+    document.getElementById('content').innerHTML = `
+                <select onchange="selectGenre(this.value)">
+                    ${optionsHtml}
+                </select>
+
+                <table>
+                    ${moviesHtml}
+                </table>
+            `;
+}
+
+
+
+
+function selectGenre(genre) {
+    selectedGenre = genre;
+    currentMovies = [];
+    for (let movie of movies) {
+        if (movie.genres.includes(genre)) {
+            currentMovies.push(movie);
+        }
+    }
+    showDiv();
+}
+
+//function extractGenres() {
+//    let genres = new Set();
+//    for (let movie of movies) {
+//        let genresOfOneMovie = movie.genres.split('|');
+//       for (let genre of genresOfOneMovie) {
+//            genres.add(genre);
+//        }
+//    }
+//    return genres;
 //}
-console.log(alle);
+
+//function extractGenres() {
+//    let genres = [];
+//    for (let movie of movies) {
+//        let genresOfOneMovie = movie.genres.split('|');
+//        for (let genre of genresOfOneMovie) {
+//            if (!genres.includes(genre)) {
+//                genres.push(genre);
+//            }
+//        }
+//    }
+//    return genres;
+//}
+
+                                    //for (let i = 0; i < movies.length; i++) {
+                                    //    let movie = movies[i];
+                                    //    // ...
+                                    //}
 
 
 
