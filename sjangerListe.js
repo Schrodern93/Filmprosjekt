55105c304da0ee55ea04a8d0e0d711da9597257d
ajja94 <anja@getacademy.no>
2019-11-15 11:37:44 +0100

    :+1:

--- sjangerListe.js
+++ sjangerListe.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 load();
 //for (i = 0; i < FilmData.length; i++) {
-    let alle = all[  FilmData.Tittel, FilmData.Sjanger,  FilmData.Spilletid, FilmData.Favoritt];
+let alle = all[{ FilmData.Tittel, FilmData.Sjanger, FilmData.Spilletid, FilmData.Favoritt }];
 
    // alle = FilmData;
 //}
