0ff2a7840ce27c3a31796ef6371b0e6acad90c51
Joakim Nikolai Schrøder <joakim@getacademy.no>
2019-11-16 00:34:12 +0100

    :-1:

--- Load.js
+++ Load.js
@@ -6,6 +6,7 @@ function load() {
         docRef.get().then(function (doc) {
             if (doc && doc.exists) {
                 let FilmData = doc.data(); // mulig const her 
+
                 //maincontent.innerHTML += '<li>' + FilmData.Tittel + ' (' + FilmData.Sjanger + ')' + '(' + FilmData.Spilletid + ' min )' + '(' + FilmData.Favoritt + ')</li>';
 
             }
