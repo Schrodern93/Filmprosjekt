1dc664abd533bd3106d254dc8d0db2621ad4c0ec
ajja94 <anja@getacademy.no>
2019-11-15 11:36:34 +0100

    :+1:

--- Load.js
+++ Load.js
@@ -5,7 +5,8 @@ function load() {
     loadButton.addEventListener("click", function () {
         docRef.get().then(function (doc) {
             if (doc && doc.exists) {
-                let FilmData = doc.data(); // mulig const her 
+                let Data = doc.data(); // mulig const her 
+                document.getElementById('mainContent').innerHTML = Data;
                 //maincontent.innerHTML += '<li>' + FilmData.Tittel + ' (' + FilmData.Sjanger + ')' + '(' + FilmData.Spilletid + ' min )' + '(' + FilmData.Favoritt + ')</li>';
 
             }
--- sjangerListe.js
+++ sjangerListe.js
@@ -1,8 +1,11 @@
 // JavaScript source code
 load();
+//for (i = 0; i < FilmData.length; i++) {
+    let alle = all[  FilmData.Tittel, FilmData.Sjanger,  FilmData.Spilletid, FilmData.Favoritt];
 
-let all [] = FilmData;
-console.log(all);
+   // alle = FilmData;
+//}
+console.log(alle);
 
 
 
