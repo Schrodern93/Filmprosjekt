57aca8c6ff6c685fe700c9c76823fb1ef7e62be5
Joakim Nikolai Schrøder <joakim@getacademy.no>
2019-11-18 20:56:19 +0100

    af

--- LeggInnFilm.js
+++ LeggInnFilm.js
@@ -1,36 +1,4 @@
-// JavaScript source code
-// Tanken her er at funksjonen som kan legge til nye filmer er her
 
-function LeggInn() {
-
-	let info = {
-		Tittel: inputTittel.value,
-		SpilleTid: inputSpilletid.value,
-		Sjanger: inputSjanger.value,
-		Favoritt: inputFavoritt.value
-	};
-	userCollection.add(info);
-}
-
-//const docRef = firestore.doc("FilmData");
-////const outputHeader = document.querySelector("#hotDogOutput");
-//const inputTittel = document.querySelector("#Tittel");
-//const inputSjanger = document.querySelector("#Sjanger");
-//const inputSpilletid = document.querySelector("#Spilletid");
-//const inputFavoritt = document.querySelector("#Favoritt");
-
-//const saveButton = document.querySelector("#saveButton");
-//saveButton.addEventListener("click", function () {
-//    const textToSave = input.value;
-//    console.log("I am going to save " + textToSave + " to Firestore");
-//    docRef.set({
-//        hotDogStatus: textToSave
-//    }).then(function () {
-//        console.log("Status saved");
-//    }).catch(function (error) {
-//        console.log("Got an error", error);
-//    });
-//});
 function showAddMovies() {
 	let movieBox = document.getElementById('addMovieBox');
 	let html = `
@@ -54,27 +22,34 @@ function showAddMovies() {
 						
 						`;
 	movieBox.innerHTML = html;
-	
 }
 
-let inputTittle = document.getElementById('inputTittel');
+let inputTittel = document.getElementById('inputTittel');
 let inputSpilletid = document.getElementById('inputSpilletid');
 let inputSjanger = document.getElementById('inputSjanger');
-let inputFavorit = document.getElementById('inputFavoritt');
+let inputFavoritt = document.getElementById('inputFavoritt');
+
+let db = firebase.firestore(); // mulig const eller var
+var userCollection = db.collection('FilmData');
+console.log(inputTittel);
 
 function LeggInn() {
-	filmData.push({
-		Tittel: inputTittle.value,
+    let info = {
+		Tittel: inputTittel.value,
 		Spilletid: parseInt(inputSpilletid.value),
 		Sjanger: inputSjanger.value,
-		Favoritt: inputFavorit.checked,
-	});
+		Favoritt: inputFavoritt.checked
+	};
+    userCollection.add(info);
+    removeMovieBox();
+}
 	//inputTittle.value = '';
 	//inputSpilletid.value = '';
 	//inputSjanger.value = '';
 	//inputFavorit.value = '';
-	removeMovieBox();
-}
+
+	
+//}
 function addMovieBox() {
 	let mContent = document.getElementById('mainContent');
 	let boxHTML = '<div id="addMovieBox"></div>';
