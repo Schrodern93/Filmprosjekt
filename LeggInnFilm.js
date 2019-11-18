a078f623cf01d118084171610c3c6f4bc7c11147
Joranstr <55440769+Joranstr@users.noreply.github.com>
2019-11-18 13:20:53 +0100

    Update LeggInnFilm.js

--- LeggInnFilm.js
+++ LeggInnFilm.js
@@ -73,24 +73,24 @@ function LeggInn() {
 	removeMovieBox();
 }
 function addMovieBox() {
-    let mContent = document.getElementById('mainContent');
-    showAddMovies()
+	let mContent = document.getElementById('mainContent');
+	showAddMovies()
 
-    let html = `
-        
-        <p class="addMovieHeader">Legg til film</p> <p class="close" onclick="removeMovieBox()">X</p>
-        <p>Her kommer Jøran's HTML</p>
-        </div>`;
+	let html = `
+		
+		<p class="addMovieHeader">Legg til film</p> <p class="close" onclick="removeMovieBox()">X</p>
+		<p>Her kommer Jøran's HTML</p>
+		</div>`;
 
-    mContent.innerHTML += html;
+	mContent.innerHTML += html;
 }
 
 function removeMovieBox() {
-    let movieBox = document.getElementById('addMovieBox');
-    movieBox.remove();
+	let movieBox = document.getElementById('addMovieBox');
+	movieBox.remove();
 }
 
 function blur(elementID) {
-    selectedClass = document.getElementById(elementID);
-    selectedClass.classList.add(blur);
+	selectedClass = document.getElementById(elementID);
+	selectedClass.classList.add(blur);
 }
