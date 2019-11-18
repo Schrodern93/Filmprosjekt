82171faa8f39515f7a6fe2bc117cc61127079ca5
Joakim Nikolai Schrøder <joakim@getacademy.no>
2019-11-18 12:57:26 +0100

    :+1:

---
+++ ShowVerdi.js
@@ -0,0 +1,28 @@
+// JavaScript source code
+showverdi();
+function showverdi() {
+    let optionsHtml = '';
+    for (let genre of genres) {
+        let selected = selectedGenre == genre ? 'selected' : '';
+        optionsHtml += `<option ${selected}>${genre}</option>`;
+    }
+
+    let moviesHtml = '';
+    for (let movie of currentMovies) {
+        moviesHtml += `<tr>
+                                    <td>${movie.movie_title}</td>
+                                    <td>${movie.genres}</td>
+                                    <td>${movie.director_name}</td>
+                                    <td>${movie.title_year}</td>
+                        </tr>`;
+    }
+    document.getElementById('content').innerHTML = `
+                <select onchange="selectGenre(this.value)">
+                    ${optionsHtml}
+                </select>
+
+                <table>
+                    ${moviesHtml}
+                </table>
+            `;
+}
