cd6daab631c4a712b6cf925a62eda4e57fb0ccdb
Mathias F B <mathias@getacademy.com>
2019-11-18 12:10:29 +0100

    showAddMovie

---
+++ showAddMovie.js
@@ -0,0 +1,23 @@
+
+function addMovieBox(){
+    let mContent = document.getElementById('mainContent');
+
+
+    let html = `
+        <div id="addMovieBox">
+        <p class="addMovieHeader">Legg til film</p> <p class="close" onclick="removeMovieBox()">X</p>
+        <p>Her kommer Jøran's HTML</p>
+        </div>`;
+    
+    mContent.innerHTML += html;
+}
+
+function removeMovieBox(){
+    let movieBox = document.getElementById('addMovieBox');
+    movieBox.remove();
+}
+
+function blur(elementID){
+    selectedClass = document.getElementById(elementID);
+    selectedClass.classList.add(blur);
+}
