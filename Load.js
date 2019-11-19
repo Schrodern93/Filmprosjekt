b6ec315de6b0c7e9ead822c92a2259721b22c30a
Mathias F B <mathias@getacademy.com>
2019-11-19 13:57:16 +0100

    IT WORKH!! :100:  :+1:  :alien:
    :}

--- Load.js
+++ Load.js
@@ -3,23 +3,39 @@ var firestore = firebase.firestore();
 const docRef = firestore.collection("FilmData");
 //const loadButton = document.querySelector("#loadButton");
 function load() {
-    
+    let gContainer = document.getElementById('gridContainer');
     let db = firebase.firestore(); // mulig const eller var
 
     var userCollection = db.collection('FilmData');
     userCollection.orderBy('Tittel').onSnapshot(
         function (snapshot) {
-            let html = '<ul>';
+            let html = '';
+            // '<ul>';
             snapshot.forEach(
                 function (FilmDataSnapshot) {
+                    
                     let FilmData = FilmDataSnapshot.data();
-                    console.log(FilmData);
-                    html += '<li>' + FilmData.Tittel + ' ' + FilmData.Spilletid + 'min ' + FilmData.Sjanger + ' ' + FilmData.Favoritt + '</li>';
+                    let filmTittel = [FilmData.Tittel];
+                    let filmSpilletid = [FilmData.Spilletid];
+                    let filmSjanger = [FilmData.Sjanger];
+                    let filmFavoritt = [FilmData.Favoritt];
 
-                    console.log(FilmDataSnapshot.data);
+                    for (i = 0; i < filmTittel.length; i++) {
+
+                        if (filmFavoritt == true) {
+                            yayOrNay = '♥';
+                        }
+                        else {
+                            yayOrNay = '♡';
+                        }
+                        html += `<div class="container">
+                                    <h3.title>${filmTittel}</h3>
+                                    <p.sjanger>${filmSjanger}</p>
+                                    <p.spilletid>${filmSpilletid}</p>
+                                    <div.favoritt>${yayOrNay}</div></div>`;
+                    }
+        
+                    gContainer.innerHTML = html;
                 });
-
-            html += '</ul>';
-            document.getElementById('mainContent').innerHTML = html;
         });
 }
